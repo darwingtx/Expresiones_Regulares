b35a28930b3d18305c1f8de58cd2ca59be34bbdc
darwingtx <dc162005@gmail.com>
2024-03-26 22:49:49 -0500

    Comentarios

--- src/Utilidades/Util.java
+++ src/Utilidades/Util.java
@@ -8,14 +8,14 @@ public class Util {
 
     }
 
+    //Dado un numero que son los digito del numero a devolver aleatorio
     public static int Llenado(int digit) {
         int min = (int) Math.pow(10, digit - 1);
         int max = (int) Math.pow(10, digit) - 1;
-        
-        
         return (int) (Math.random() * (max - min + 1)) + min;
     }
 
+    //Da un numero primo
     public int Primo(){
         int n = (int) (Math.random() * 20 + 1);
         if (isPrimo(n) == true){
