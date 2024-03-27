bcaf6130ed260b5a78ce42500691c48fa65fb382
Andrés Álvarez <andravid019@gmail.com>
2024-03-27 09:18:45 -0500

    creacion clase1

---
+++ src/Array_1.java
@@ -0,0 +1,31 @@
+
+
+import java.util.ArrayList;
+
+
+public class Array_1 {
+    public static ArrayList<String> Array_1 = new ArrayList<>();
+    private double[] vect;
+
+    
+    public Array_1() {
+        //implementar el llenado del vector desde aca mismo para evitar el uso de mas codigo
+    }
+    public static ArrayList<String> getArray_1() {
+        return Array_1;
+    }
+    public static void setArray_1(ArrayList<String> array_1) {
+        Array_1 = array_1;
+    }
+    public double[] getVect() {
+        return vect;
+    }
+    public void setVect(double[] vect) {
+        this.vect = vect;
+    }
+
+    
+
+
+
+}
