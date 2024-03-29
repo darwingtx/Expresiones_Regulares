b74c05726d4f9d05071963eca0d64c35ba1c63e8
Andrés Álvarez <andravid019@gmail.com>
2024-03-29 18:26:08 -0500

    solo falta ultimo item

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,24 @@ import Utilidades.Util;
     public class Main {
 
        public static void main(String[] args) {
-        Array_1 x = new Array_1();
-        x.llenarArray_1();
-        ArrayList<Array_1> T = Array_1.getArray_1();
+       Array_3 x = new Array_3();
+       x.CargarDatos("D:\\Users\\Usuario\\Documents\\NetBeansProjects\\AP4\\Expresiones_Regulares\\src\\Utilidades\\cargabasica.txt");
+       x.agregarObj();
+       ArrayList<Array_3> T = Array_3.Array_3;
+       x.buscarTd();
+       
+       
+       }
+    }
+
+    /* //manera de mostrar
+     ArrayList<Array_1> T = Array_1.getArray_1();
         for (int i = 0; i < T.size(); i++) {
            for(int j=0;j<3;j++){
                    System.out.println( T.get(i).getVect(j));
            }
         }
      
-       }
-    }
-
-    /* //manera de mostrar
     */
 
    
