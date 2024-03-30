1e1e280e32f48a2e333832dce0bb2d813f698212
darwingtx <dc162005@gmail.com>
2024-03-29 21:32:50 -0500

    Merge

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,10 @@ public class Main {
        x.agregarObj();
        ArrayList<Array_3> T = Array_3.Array_3;
        x.buscarTd();
+       Array2 x = new Array2();
+       x.LlenarArray2();
+       x.Listar();
+       x.NumeroMayor();
        
        
        }
@@ -29,19 +33,4 @@ public class Main {
     */
 
    
-    public static void main(String[] args) {
-        // Array_1 x = new Array_1();
-        // x.llenarArray_1();
-        // ArrayList<Array_1> T = Array_1.getArray_1();
-        // for (int i = 0; i < T.size(); i++) {
-        // for(int j=0;j<3;j++){
-        // System.out.println( T.get(i).getVect(j));
-        // }
-        // }
-
-        Array2 x = new Array2();
-        x.LlenarArray2();
-        x.Listar();
-        x.NumeroMayor();
-    }
-}
+    
