411823e9514f826b95b43c735b109492097ae43c
Andrés Álvarez <andravid019@gmail.com>
2024-04-02 11:59:47 -0500

    Array_1 solucionado

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -2,81 +2,129 @@ package Menu;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-import arrayLogic.*; 
+import arrayLogic.*;
 
 public class Menu {
 
     public void menu() {
-      
-        int op = Integer.parseInt(JOptionPane.showInputDialog(null, "-----Menu de Arrays-----\n"
-                + "1.Array 1\n"
-                + "2.Array 2\n"
-                + "3.Array 3\n"
-                + "4.Array 4\n"
-                + "5.Array 5\n"
-                + "0.Salir\n"));
-        switch (op) {
-            case 1:
-            menuArray1();
-                break;
-            case 2:
+        int op;
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(null, "-----Menu de Arrays-----\n"
+                    + "1.Array 1\n"
+                    + "2.Array 2\n"
+                    + "3.Array 3\n"
+                    + "4.Array 4\n"
+                    + "5.Array 5\n"
+                    + "0.Salir\n"));
+            switch (op) {
+                case 1:
+                    menuArray1();
+                    break;
+                case 2:
 
-                break;
+                    break;
 
-            case 3:
+                case 3:
+                    menuArray3();
+                    break;
 
-                break;
+                case 4:
 
-            case 4:
+                    break;
+                case 5:
 
-                break;
-            case 5:
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Hasta luego");
+                    break;
 
-                break;
-            case 6:
+                default:
+                    break;
+            }
 
-                break;
-
-            default:
-                break;
-        }
-
-    }
-
-    private void menuArray1(){
-        ArrayList<Array_1> x = new ArrayList<>();
-        int op = Integer.parseInt(JOptionPane.showInputDialog(null, "-----Menu de Array 1-----\n"
-                + "1.Crear el Array.\n"
-                + "2.Mostrar.\n"
-                + "3.Promedio de los valores\n"
-                + "0.Salir\n"));
-        switch (op) {
-            case 1:
-                 Array_1.llenarArray_1();
-                 JOptionPane.showMessageDialog(null, "El Array ha sido llenado");
-                break;
-            case 2:
-                Array_1.mostrar();
-                break;
-
-            case 3:
-
-                break;
-
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-
-                break;
-
-            default:
-                break;
-        }
+        } while (op != 0);
 
     }
 
+    private void menuArray1() {
+        int op;
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(null, "-----Menu de Array 1-----\n"
+                    + "1.Crear el Array.\n"
+                    + "2.Mostrar.\n"
+                    + "3.Promedio de los valores\n"
+                    + "0.Salir\n"));
+            switch (op) {
+                case 1:
+                    Array_1.llenarArray_1();
+                    JOptionPane.showMessageDialog(null, "El Array ha sido llenado");
+                    break;
+                case 2:
+                    if (!Array_1.getArray_1().isEmpty()) {
+                        Array_1.mostrar();
+                    } else
+                        JOptionPane.showMessageDialog(null, "El Array esta vacio");
+                    break;
+
+                case 3:
+                    if (!Array_1.getArray_1().isEmpty()) {
+                        Array_1.promedio();
+                    } else
+                        JOptionPane.showMessageDialog(null, "El Array esta vacio");
+                    break;
+
+                case 0:
+                JOptionPane.showMessageDialog(null, "Volviendo...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion incorrecta");
+                    break;
+            }
+        } while (op != 0);
+    }
+
+    private void menuArray3() {
+        int op;
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog(null, "-----Menu de Array 3-----\n"
+                    + "1.Cargar datos en el Array.\n"
+                    + "2.Agregar Docente.\n"
+                    + "3.Mostrar.\n"
+                    + "4.Buscar e imprimir el porcentaje de cierto docente dado.\n"
+                    + "5.Cantidad de veces que se equivoco el usuario ingresando el numero de cedula.\n"
+                    + "6.Buscar y almacenar en otro ArrayList los docentes de catedra que almacenaron la fecha con mes.\n"
+                    + "0.Salir\n"));
+            switch (op) {
+                case 1:
+                    Array_3.CargarDatos(
+                            "D:\\Users\\Usuario\\Documents\\NetBeansProjects\\AP4\\Expresiones_Regulares\\src\\Utilidades\\cargabasica.txt");
+                    JOptionPane.showMessageDialog(null, "El Array ha sido llenado");
+                    break;
+                case 2:
+                    Array_3.agregarObj();
+                    break;
+
+                case 3:
+                 Array_3.mostrar(Array_3.Array_3);
+                    break;
+                case 4:
+                    Array_3.buscarTd();
+                    break;
+                case 5:
+                    Array_3.errores();
+                    break;
+
+                case 6:
+                Array_3.docenteMes();
+                    break;
+
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Volviendo...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion incorrecta");
+                    break;
+            }
+        } while (op != 0);
+    }
 }
--- src/arrayLogic/Array_1.java
+++ src/arrayLogic/Array_1.java
@@ -1,82 +1,99 @@
 package arrayLogic;
 
-
-
 import java.util.*;
-
 import javax.swing.JOptionPane;
 import javax.swing.text.html.HTMLDocument.Iterator;
 
 import Utilidades.Util;
 
-
 public class Array_1 {
     public static ArrayList<Array_1> Array_1 = new ArrayList<>();
     private long[] vect = new long[3];
 
-    
     public Array_1() {
-    
+
     }
-    
-    public static void llenarArray_1(){
+
+    public static void llenarArray_1() {
+        if(!Array_1.isEmpty()){
+            Array_1.clear();
+        }
         Util x = new Util();
-        int i = 4;//x.Primo();
+        int i = x.Primo();
         for (int index = 0; index < i; index++) {
-         Array_1 z = new Array_1();
-         z.setVect(0, verificacion(index+1,'0'));
-         z.setVect(1, verificacion(index+1, Long.toString(z.getVect(0)).charAt(0)));
-         z.setVect(2, verificacion(index+1,  Long.toString(z.getVect(1)).charAt(0)));
-         Array_1.add(z);
+            Array_1 z = new Array_1();
+            long llenado = verificacion(index + 1,'0');
+            String llenx = Character.toString(Long.toString(llenado).charAt(0));;
+            while (!llenx.matches("[0-7]") || llenado == 0){
+                llenado = verificacion(index + 1,'0');
+                llenx = Character.toString(Long.toString(llenado).charAt(0));;
+            }
+            z.setVect(0, llenado);
+            llenado = verificacion(index + 1,Long.toString(z.getVect(0)).charAt(0));
+            llenx = Character.toString(Long.toString(llenado).charAt(0));;
+            while (!llenx.matches("[0-8]") || llenado == 0) {
+                llenado = verificacion(index + 1,Long.toString(z.getVect(0)).charAt(0));
+                llenx = Character.toString(Long.toString(llenado).charAt(0));;
+            }
+            z.setVect(1, llenado);
+            llenado = verificacion(index + 1,Long.toString(z.getVect(1)).charAt(0));
+            llenx = Character.toString(Long.toString(llenado).charAt(0));;
+            while (!llenx.matches("[0-9]") || llenado == 0) {
+                llenado = verificacion(index + 1,Long.toString(z.getVect(1)).charAt(0));
+                llenx = Character.toString(Long.toString(llenado).charAt(0));;
+            }
+            z.setVect(2, llenado);
+            Array_1.add(z);
         }
 
     }
-   
+    
     private static int verificacion(int i, char r){
-            int num = Util.Llenado(i);
-            String expr = "["+r+"-9]{"+i+"}";
-            String valor = ""+num;
+        int num = Util.Llenado(i);
+        String expr = "["+r+"-9]{"+i+"}";
+        String valor = ""+num;
 
-        if(valor.matches(expr)){
-            return num;
-        }
-        return verificacion(i,r);
+    if(valor.matches(expr)){
+        return num;
     }
+    return 0;
+}
 
-    public void promedio(){
-        if(Array_1.size()%2==0){
+    public static void promedio() {
+        if (Array_1.size() % 2 == 0) {
             promediopar();
-        }else{
+        } else {
             promedioImpar();
         }
     }
-       
-    public static void  mostrar(){
+
+    public static void mostrar() {
         ArrayList<Array_1> T = Array_1;
         String s = "";
         for (int i = 0; i < T.size(); i++) {
-            s+= "[";
-           for(int j=0;j<3;j++){
+            s += "[";
+            for (int j = 0; j < 3; j++) {
 
-                  s+= T.get(i).getVect(j)+", ";
-           }
-           s+="]\n";
+                s += T.get(i).getVect(j) + ",";
+            }
+            s += "]\n";
         }
         JOptionPane.showMessageDialog(null, s);
-        }
-    private void promedioImpar(){
-        float prom=0;
+    }
+
+    private static void promedioImpar() {
+        float prom = 0;
         String cadP = "-";
         int j = 0;
         Array_1 z = Array_1.get(j);
-        for (int i = 0; i < Array_1.size(); i+=2) {
+        for (int i = 0; i < Array_1.size(); i += 2) {
             z = Array_1.get(j);
-            prom = z.getVect(0)+z.getVect(1)+z.getVect(2);
-            cadP += prom/3+"-";
-            if(i+1!=Array_1.size()){
-            z = Array_1.get((Array_1.size()-1)-j);
-            prom = z.getVect(0)+z.getVect(1)+z.getVect(2);
-            cadP+=prom/3+"-";
+            prom = z.getVect(0) + z.getVect(1) + z.getVect(2);
+            cadP += prom / 3 + "-";
+            if (i + 1 != Array_1.size()) {
+                z = Array_1.get((Array_1.size() - 1) - j);
+                prom = z.getVect(0) + z.getVect(1) + z.getVect(2);
+                cadP += prom / 3 + "-";
             }
             j++;
         }
@@ -84,17 +101,17 @@ public class Array_1 {
 
     }
 
-    private void promediopar(){
-        float prom=0;
+    private static void promediopar() {
+        float prom = 0;
         String cadP = "-";
         int j = 0;
-        for (int i = 0; i < Array_1.size(); i+=2) {
+        for (int i = 0; i < Array_1.size(); i += 2) {
             Array_1 z = Array_1.get(j);
-            prom = (z.getVect(0)+z.getVect(1)+z.getVect(2))/3;
-            cadP += prom+"-";
-            z = Array_1.get((Array_1.size()-1)-j);
-            prom = z.getVect(0)+z.getVect(1)+z.getVect(2);
-            cadP+=prom/3+"-";
+            prom = (z.getVect(0) + z.getVect(1) + z.getVect(2)) / 3;
+            cadP += prom + "-";
+            z = Array_1.get((Array_1.size() - 1) - j);
+            prom = z.getVect(0) + z.getVect(1) + z.getVect(2);
+            cadP += prom / 3 + "-";
             j++;
         }
         JOptionPane.showMessageDialog(null, cadP);
@@ -104,19 +121,19 @@ public class Array_1 {
     public static ArrayList<Array_1> getArray_1() {
         return Array_1;
     }
-    
+
     public long getVect(int i) {
         return vect[i];
     }
-    
+
     public long[] getVect() {
         return vect;
     }
-    
+
     public void setVect(long vect[]) {
-        this.vect = vect ;
+        this.vect = vect;
     }
-    
+
     public void setVect(int n, long i) {
         this.vect[n] = i;
     }
